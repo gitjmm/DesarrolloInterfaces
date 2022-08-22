61cc5c479326ad14fc8cb77273223bc5cd610a90
Jorge Martínez <jmmostazo.medac@gmail.com>
2022-08-22 12:26:02 +0200

    Segundo commit. Prueba

--- DesarrolloInterfaces/src/main/java/com/mycompany/desarrollointerfaces/DesarrolloInterfaces.java
+++ DesarrolloInterfaces/src/main/java/com/mycompany/desarrollointerfaces/DesarrolloInterfaces.java
@@ -12,6 +12,6 @@ package com.mycompany.desarrollointerfaces;
 public class DesarrolloInterfaces {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World!");
     }
 }
