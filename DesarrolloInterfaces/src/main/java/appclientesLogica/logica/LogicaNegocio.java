d2c5c9ff4eae0322dc17aa898d26caa4a1a7a58a
Jorge Martínez <jmmostazo.medac@gmail.com>
2022-08-25 11:06:25 +0200

    Primera Aplicacion

---
+++ DesarrolloInterfaces/src/main/java/appclientesLogica/logica/LogicaNegocio.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package appclientesLogica.logica;
+
+import appclientesLogica.clases.Cliente;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Jorge Martínez
+ * Esta clase se va a encargar de las operaciones correspondietes: crear,borrar...
+ */
+public class LogicaNegocio {
+    private static ArrayList<Cliente> listaClientes = new ArrayList<>();
+    
+    public static void addCliente(Cliente cliente){
+        listaClientes.add(cliente);
+    }
+
+    public static ArrayList<Cliente> getListaClientes() {
+        return listaClientes;
+    }
+    
+    
+    
+}
